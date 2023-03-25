e34a42904fa55d2dd4d7a6b5141d2e6f019f3a2a
Oscar Otero <oom@oscarotero.com>
2023-03-25 14:21:56 +0100

    moved to deno deploy

--- _config.ts
+++ _config.ts
@@ -2,7 +2,7 @@ import lume from "lume/mod.ts";
 import jsx from "lume/plugins/jsx.ts";
 import lightningCss from "lume/plugins/lightningcss.ts";
 
-const location = new URL("https://oscarotero.com/deno/");
+const location = new URL("https://cheatsheet.deno.dev/");
 
 export default lume({ location })
   .use(jsx())
