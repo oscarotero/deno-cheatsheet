6443f6436a1ca512a40a88bd13dd0b9c1ea3f85c
Oscar Otero <oom@oscarotero.com>
2023-05-15 12:38:35 +0200

    improved tools docs. Fixed std library links

--- _components/deno/tool.tsx
+++ _components/deno/tool.tsx
@@ -1,64 +1,22 @@
 interface Props {
-  title: string;
-  cli?: string;
-  api?: string;
-  stable?: boolean;
-  config?: string;
+  cli: string;
+  links?: {
+    text: string;
+    url: string;
+  }[];
 }
 
-interface Helpers {
-  md: (content: string) => string;
-}
-
-export default function doc(
-  { cli, api, config, stable }: Props,
-  { md }: Helpers,
-) {
-  const configHtml = config ? md(config) : null;
-
+export default function doc({ cli, links }: Props) {
   return (
     <>
-      {cli && (
-        <pre>
-          <code>{cli}</code>
-        </pre>
-      )}
-      {api && <Api name={api} stable={stable} />}{" "}
-      {configHtml && <Config content={configHtml} />}
-    </>
-  );
-}
-
-interface ApiProps {
-  name: string;
-  stable?: boolean;
-}
-
-function Api({ name, stable }: ApiProps) {
-  const href = `https://doc.deno.land/deno/${
-    stable ? "stable" : "unstable"
-  }/~/${name}`;
-
-  return <a className="button" href={href} data-modal>{name}()</a>;
-}
-
-interface ConfigProps {
-  content: string;
-}
-function Config({ content }: ConfigProps) {
-  return (
-    <>
-      <button data-dialog className="button config-button">Config</button>
-      <dialog className="config-dialog">
-        <nav>
-          <button arial-label="Close">✕</button>
-        </nav>
-        <div
-          className="config-content"
-          dangerouslySetInnerHTML={{ __html: content }}
-        >
-        </div>
-      </dialog>
+      <pre>
+        <code>{cli}</code>
+      </pre>
+      {
+        links?.map((link) => 
+          <a href={link.url} data-modal>{link.text}</a>
+        )
+      }
     </>
   );
 }
